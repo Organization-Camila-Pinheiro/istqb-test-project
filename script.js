09731ec04b83731c0fe14db774e27d30f00d5b35
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-05-15 15:25:07 +0200

    IOT_2 Show the answer
    https://camilaqa.atlassian.net/browse/IOT-2

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ function bienvenida(){
     let nombre = document.getElementById("nombre1").value;
 
     let mensaje = "Hola " + nombre + ", sea bienvenida al examen de ISTQB! Que hagas un buen examen!"
+    imprimir(mensaje, "welcome1")
+}
+
+function showAnswer(){
+    let mensaje = "La respuesta correcta es la C " 
     imprimir(mensaje, "respuesta1")
 }

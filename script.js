cbd18f2f48856515cba4c7d153f8599d3d3051b1
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-05-27 15:18:19 +0200

    IOT -3 and IOT-4
    https://camilaqa.atlassian.net/browse/IOT-3
    https://camilaqa.atlassian.net/browse/IOT-4

--- script.js
+++ script.js
@@ -6,12 +6,42 @@ parrafo.innerText = mensaje
 function bienvenida(){
     let nombre = document.getElementById("nombre1").value;
 
-    let mensaje = "Hola " + nombre + ", sea bienvenida al examen de ISTQB! Que hagas un buen examen!"
-    imprimir(mensaje, "welcome1")
+    let mensaje = "Hola " + nombre + ", sea bienvenida al examen de ISTQB! Que hagas un buen examen!";
+    imprimir(mensaje, "welcome1");
 }
 
 function showAnswer(){
-    let answer = "C"
-    let mensaje = "La respuesta correcta es la " + answer
-    imprimir(mensaje, "respuesta1")
+    let answer = "C";
+    let mensaje = "La respuesta correcta es la " + answer;
+    imprimir(mensaje, "respuesta1");
 }
+
+function correctAnswer(){
+
+    let answerCorrect = "C";
+    let emptyAnswer = ""
+    let answerStudent = document.getElementById("pregunta1").value;
+    let mensaje = ""
+    let score = 0
+
+    if(answerStudent === emptyAnswer ){
+        score = 0
+        mensaje = "Respuesta equivodada! Puntuación: " + score;
+
+    }
+    else if(answerStudent === answerCorrect){
+        score = 2
+        mensaje = "Respuesta correcta! Puntuación: " + score;
+    }
+
+    else if(answerStudent !== answerCorrect){
+        score = -1
+        mensaje = "Respuesta equivodada! Puntuación: " + score;
+    }
+    
+    imprimir(mensaje, "respuesta1");
+
+    }
+    
+    
+

171dc75b9ec19628c4596f61ab4719b7bd685319
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-06-03 13:00:08 +0200

    IOT-5
    https://camilaqa.atlassian.net/browse/IOT-5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+listaRespuesta = ["C", "C", "A", "B", "C", "A", "C", "A", "B", "C"]
+
 function imprimir(mensaje, id){
 let parrafo = document.getElementById(id)
 parrafo.innerText = mensaje
@@ -10,17 +12,17 @@ function bienvenida(){
     imprimir(mensaje, "welcome1");
 }
 
-function showAnswer(){
-    let answer = "C";
+function showAnswer(numeroPregunta){
+    let answer = listaRespuesta[numeroPregunta -1];
     let mensaje = "La respuesta correcta es la " + answer;
-    imprimir(mensaje, "respuesta1");
+    imprimir(mensaje, "respuesta"+numeroPregunta);
 }
 
-function correctAnswer(){
+function correctAnswer(numeroPregunta){
 
-    let answerCorrect = "C";
+    let answerCorrect = listaRespuesta[numeroPregunta-1];
     let emptyAnswer = ""
-    let answerStudent = document.getElementById("pregunta1").value;
+    let answerStudent = document.getElementById("pregunta"+numeroPregunta).value;
     let mensaje = ""
     let score = 0
 
@@ -39,31 +41,7 @@ function correctAnswer(){
         mensaje = "Respuesta equivodada! Puntuación: " + score;
     }
     
-    imprimir(mensaje, "respuesta1");
+    imprimir(mensaje, "corrigir"+numeroPregunta);
 
     }
     
-
-
-    let lista = ["B. Pruebas de Regresión","C. Todos aquellos que puedan tener algún uso para la herramienta","A. Requerimientos", "A. Analista funcional", "A. Diseño de Prueba","C. Calculos incorrectos", "C. Validan que el sistema esté completo", "B. Prueba de Aceptación de Usuario", "A. Evaluan las funciones que el sistema debe realizar", "A. Se  basan en la estructura interna del sistema"];
-
-
-function mostrarrespuesta(numeroPregunta) {
-    let respuestaSeleccionada = document.getElementById("pregunta"+numeroPregunta).value;
-    let mensaje = "Su respuesta: " + respuestaSeleccionada;
-    imprimir(mensaje, "hacontestado"+numeroPregunta);
-    
-    let mensaje2 = "La respuesta correcta es " + lista[numeroPregunta-1];
-    imprimir(mensaje2, "mostrarrespuesta"+numeroPregunta);
-
-}
-
-let respuetas = ["B","C","A"]
-
-function mostrarrespuesta(numeroPregunta) {
-    let respuestaCorrecta = lista[numeroPregunta-1];
-    let mensaje = "La respuesta correcta es " + respuestaCorrecta;
-    imprimir(mensaje,"mostrarRespuesta"+numeroPregunta);
-}
-    
-

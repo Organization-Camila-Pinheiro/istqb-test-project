d7595a2aede3b0668fda84d562d92a00e63053d2
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-06-28 18:32:43 +0200

    IOT -7 Ranking
    https://camilaqa.atlassian.net/browse/IOT-7

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 listaRespuesta = ["C", "C", "A", "B", "C", "A", "C", "A", "B", "C"]
 listaRanking = []
+let sumaTotal = 0
+let nombreEstudiante = ""
 
 function imprimir(mensaje, id) {
     let parrafo = document.getElementById(id)
@@ -7,14 +9,14 @@ function imprimir(mensaje, id) {
 }
 
 function bienvenida() {
-    let nombre = document.getElementById("nombre1").value;
+    nombreEstudiante = document.getElementById("nombre1").value;
 
     let mensaje = "Hola " + nombre + ", sea bienvenida al examen de ISTQB! Que hagas un buen examen!";
     imprimir(mensaje, "welcome1");
 }
 
 function showAnswer(numeroPregunta) {
-    let answer = listaRespuesta[numeroPregunta -1];
+    let answer = listaRespuesta[numeroPregunta - 1];
     let mensaje = "La respuesta correcta es la " + answer;
     imprimir(mensaje, "respuesta" + numeroPregunta);
 }
@@ -46,40 +48,69 @@ function correctAnswer(numeroPregunta) {
     return score
 }
 
-let sumaTotal = 0
 
-function sumaPuntos() {
+
+function verPontuacion() {
     let sumaTotal = 0
     let mensaje = ""
-
     for (let i = 1; i <= 10; i++) {
         let score = correctAnswer(i)
         sumaTotal += score
         mensaje = "Su pontuación final es " + sumaTotal
     }
-
-    imprimir(mensaje, "pontuacionFinal")
+    imprimir(mensaje, "pontuacionFinal");
+    agregarEstudiante();
+    borrarRespuesta();
     
+
 }
 
+function borrarRespuesta() {
+    document.getElementById("Formulario1").reset();
+    document.getElementById("formulariotest").reset();
+    document.getElementById("welcome1").innerText = "";
 
-function ranking() {
-    let student = {}
-    student.nombre = document.getElementById("nombre1").value;
-    student.notaFinal = sumaTotal;
-    listaRanking.push(student);
-    listaRanking.sort((a,b) => b.notaFinal - a.notaFinal )
-
-    let mensaje = "";
-    for (i = 0; i<listaRanking.length; i++) {
-        let studentRanking = listaRanking[i]
-        mensaje += " Nombre: " + listaRanking[i].nombre;
-        mensaje += " Pontuación final: " + listaRanking[i].notaFinal;
+    for (let i = 0; i > listaRespuesta.length; i++) {
+        document.getElementById("respuesta" + (i + 1)).innerText = "";
+        document.getElementById("corrigir" + (i + 1)).innerText = "";
     }
 
-    imprimir(mensaje, "ranking1")
-    
 }
+
+function agregarEstudiante() {
+
+    if (nombreEstudiante != "") {
+        let student = {
+            nombre: nombreEstudiante,
+            notaFinal: sumaTotal
+        }
+
+        listaRanking.push(student);
+    }
+
+    imprimirAlumno();
+}
+
+
+function ordenarRanking() {
+    listaRanking.sort((a, b) => b.notaFinal - a.notaFinal)
+}
+
+
+function imprimirAlumno() {
+    ordenarRanking();
+    let mensaje = "";
+
+    for (i = 0; i < listaRanking.length; i++) {
+        let studentRanking = listaRanking[i]
+        mensaje += "\n Nombre: " + studentRanking.nombre;
+        mensaje += "\n Pontuación final: " + studentRanking.notaFinal;
+    }
+    imprimir(mensaje, "ranking1")
+
+}
+
+
 
 
 
